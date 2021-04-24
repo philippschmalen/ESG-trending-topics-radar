63dd204e98a65b1ea18b3724316ded6c766018a0
philippschmalen <philippschmalen@gmail.com>
2021-04-24 09:06:56 +0200

    fix write-to-csv() filename

--- pipelines/esg_trending_topics/load.py
+++ pipelines/esg_trending_topics/load.py
@@ -4,7 +4,7 @@ import os
 
 def write_to_csv(df, filename):
 	# if file does not exist write header 
-	if not os.path.isfile('filename.csv'):
+	if not os.path.isfile(f'{filename}'):
 		df.to_csv(f'{filename}', index=False) 
 	else: # else it exists so append without writing the header
 		df.to_csv(f'{filename}', index=False, mode='a') 
--- test/test_script.py
+++ test/test_script.py
@@ -1,4 +1,3 @@
 from datetime import datetime
 
-print(datetime.strptime("20210424-040000UTC", "%Y%m%d-%H%M%S%Z"))
-print(datetime.now())
+datetime.strptime("20210424-040000UTC", "%Y%m%d-%H%M%S%Z")
