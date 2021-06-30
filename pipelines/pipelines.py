8b54f5a6a9612e39c5c9b07fa7ba72463a9a2272
philippschmalen <philippschmalen@gmail.com>
2021-06-30 15:52:40 +0200

    added interval

--- pipelines/pipelines.py
+++ pipelines/pipelines.py
@@ -55,7 +55,7 @@ def export(df, path):
 def load_all_query_data(raw_data_dir, filename, blacklist):
     """Loads data from previous queries """
     df_raw = analysis_extract.load_data(
-        raw_data_dir=raw_data_dir, filename=filename)
+    raw_data_dir=raw_data_dir, filename=filename)
     df = transform.clean_data(df_raw, blacklist=blacklist)
     return df
 
@@ -79,9 +79,9 @@ def transform_plot_data(df, to_uppercase, top_n):
 @task
 def web_deployment(df_trend, df_top, project_name, top_n):
     figure = deploy.create_plot(df_trend, df_top)
-    # deploy.deploy_plot(figure, filename=project_name + '_all')
+    deploy.deploy_plot(figure, filename=project_name + '_all')
     figure = deploy.create_plot_rising(df_trend)
-    # deploy.deploy_plot(figure, filename=project_name)
+    deploy.deploy_plot(figure, filename=project_name)
 
 
 def main():
@@ -108,13 +108,12 @@ def main():
 
     # ~----------------- FLOW -----------------~
     # ~-- daily schedule
-    # schedule = IntervalSchedule(
-    #     start_date=datetime.strptime("20210424-040000UTC", "%Y%m%d-%H%M%S%Z"),
-    #     # start_date=datetime.utcnow() + timedelta(seconds=1),
-    #     interval=timedelta(days=1),
-    # )
+    schedule = IntervalSchedule(
+        start_date=datetime.strptime("20210424-040000UTC", "%Y%m%d-%H%M%S%Z"),
+        interval=timedelta(days=1),
+    )
 
-    with Flow("etl") as flow: #, schedule=schedule
+    with Flow("etl", schedule=schedule) as flow: #
 
         # ~-- parameter
         raw_data_dir = Parameter(name="raw_data_dir")
